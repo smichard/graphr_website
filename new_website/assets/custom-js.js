f54caefef7043c83a123fe1d6859f53049208a16
Stephan Michard <stephan.michard@gmail.com>
2017-06-30 00:44:52 +0200

    added custom graphR logo

--- new_website/assets/custom-js.js
+++ new_website/assets/custom-js.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
         if ($(".navbar").offset().top > 50) {
-            $('.navbar-axy img').attr('src','assets/images/nav_logo_2.jpg'); //change src
+            $('.navbar-axy img').attr('src','assets/images/nav_logo_2.png'); //change src
         } else {
-            $('.navbar-axy img').attr('src','assets/images/nav_logo_1.jpg')
+            $('.navbar-axy img').attr('src','assets/images/nav_logo_1.png')
         }   
 });
