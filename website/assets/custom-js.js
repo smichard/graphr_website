0c016f8e9240add70aa02d7ee282d6940fe0f55f
Stephan Michard <stephan.michard@gmail.com>
2017-10-13 11:12:58 +0200

    version branch created

--- website/assets/custom-js.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $('.navbar-axy img').attr('src','assets/images/nav_logo_2.png'); //change src
-        } else {
-            $('.navbar-axy img').attr('src','assets/images/nav_logo_1.png')
-        }   
-});
